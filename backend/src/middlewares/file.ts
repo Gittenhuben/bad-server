42b32806a5960ffbc1f925f5596a6f4c1d63f9c0
Andrey <ae55@yandex.ru>
2025-04-22 16:22:18 +0300

    Fix 28

--- backend/src/middlewares/file.ts
+++ backend/src/middlewares/file.ts
@@ -2,17 +2,18 @@ import multer from 'multer'
 import { join } from 'path'
 import { UPLOAD } from '../config'
 
-const storage = multer.diskStorage({
-    destination: (_req, _file, cb) => cb(null, join(__dirname, `../public/${UPLOAD.temp}`))
-})
-
+/*const storage = multer.diskStorage({
+    //destination: (_req, _file, cb) => cb(null, join(__dirname, `../public/${UPLOAD.temp}`))
+    destination: (_req, _file, cb) => cb(null, join(__dirname, `../public`))
+})*/
+/*
 const types = [
     'image/png',
     'image/jpg',
     'image/jpeg',
     'image/gif',
     'image/svg+xml',
-]
+]*/
 /*
 const fileFilter: multer.Options['fileFilter'] = (_req, file, cb) => {
     cb(null, types.includes(file.mimetype))
@@ -22,4 +23,5 @@ const limits = {
     fileSize: 10 * 1024 * 1024
 }
 */
-export default multer({ storage/*, fileFilter, limits*/ })
+//export default multer({ storage/*, fileFilter, limits*/ })
+export default multer({ /*storage*//*, fileFilter, limits*/ })
