5c9b3b6137712f3080a50b1bb8adaec96d9fd112
Andrey <ae55@yandex.ru>
2025-04-23 00:51:11 +0300

    Fix 31

--- backend/src/middlewares/file.ts
+++ backend/src/middlewares/file.ts
@@ -4,24 +4,35 @@ import { UPLOAD } from '../config'
 
 const storage = multer.diskStorage({
     destination: (_req, _file, cb) => cb(null, join(__dirname, `../public/${UPLOAD.temp}`))
-    //destination: (_req, _file, cb) => cb(null, join(__dirname, `../public`))
 })
-/*
+
 const types = [
     'image/png',
     'image/jpg',
     'image/jpeg',
     'image/gif',
     'image/svg+xml',
-]*/
-/*
+]
+
 const fileFilter: multer.Options['fileFilter'] = (_req, file, cb) => {
+    if (file.size < 2 * 1024) {
+        return cb(new Error('Файл слишком мал'));
+    }
+    if (file.size > 10 * 1024 * 1024) {
+        return cb(new Error('Файл слишком велик'));
+    }
     cb(null, types.includes(file.mimetype))
 }    
 
-const limits = {
+/*const limits = {
     fileSize: 10 * 1024 * 1024
-}
-*/
-//export default multer({ storage/*, fileFilter, limits*/ })
-export default multer({ storage/*, fileFilter, limits*/ })
+}*/
+/*
+const fileFilter = (req, file, cb) => {
+    if (file.size < minSize) {
+      return cb(new Error('Файл слишком мал'), false);
+    }
+    cb(null, true);
+  };*/
+
+export default multer({ storage, fileFilter/*, limits*/ })
