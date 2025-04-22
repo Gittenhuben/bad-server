78d673f3872991836752b14e1d520aebff31f1fd
Andrey <ae55@yandex.ru>
2025-04-22 16:12:40 +0300

    Fix 27

--- backend/src/routes/upload.ts
+++ backend/src/routes/upload.ts
@@ -4,6 +4,6 @@ import fileMiddleware from '../middlewares/file'
 
 const uploadRouter = Router()
 
-uploadRouter.post('/', fileMiddleware.single('file'), uploadFile)
+uploadRouter.post('/', /*fileMiddleware.single('file'),*/ uploadFile)
 
 export default uploadRouter
